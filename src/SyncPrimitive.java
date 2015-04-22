4ccb9b74edbdddda82dddc8a70e8bccb1e9dd89f
y00s <yosyes@gmail.com>
2015-04-22 22:12:07 +0300

    last changes and bug fix

--- src/SyncPrimitive.java
+++ src/SyncPrimitive.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
@@ -16,242 +14,191 @@ import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
+/**
+ * Primitive class that synchronize with ZooKeeper implemented for both Producer
+ * and Consumer
+ * 
+ * mostly copied from the ZooKeeper example, some changes where made to support
+ * string queue
+ * 
+ * Yosef
+ */
 public class SyncPrimitive implements Watcher {
 
-    static ZooKeeper zk = null;
-    static Integer mutex;
+	// Global variables:
+	static ZooKeeper zk = null;
+	static Integer mutex;
+	String root;
 
-    String root;
+	/**
+	 *
+	 * Default constructor: if a connection does not exist start a new one and
+	 * set mutex to -1
+	 * 
+	 * Yosef
+	 */
+	SyncPrimitive(String address) {
+		if (zk == null) {
+			try {
+				System.out.println("Starting ZK:");
+				zk = new ZooKeeper(address, 3000, this);
+				mutex = new Integer(-1);
+				System.out.println("Finished starting ZK: " + zk);
+			} catch (IOException e) {
+				System.out.println(e.toString());
+				zk = null;
+			}
+		}
+	}
 
-    SyncPrimitive(String address) {
-        if(zk == null){
-            try {
-                System.out.println("Starting ZK:");
-                zk = new ZooKeeper(address, 3000, this);
-                mutex = new Integer(-1);
-                System.out.println("Finished starting ZK: " + zk);
-            } catch (IOException e) {
-                System.out.println(e.toString());
-                zk = null;
-            }
-        }
-        //else mutex = new Integer(-1);
-    }
+	/**
+	 *
+	 * Implementation of Watcher so the process get synchronization with respect
+	 * to the mutex
+	 * 
+	 * Yosef
+	 */
+	synchronized public void process(WatchedEvent event) {
+		synchronized (mutex) {
+			// System.out.println("Process: " + event.getType());
+			mutex.notify();
+		}
+	}
 
-    synchronized public void process(WatchedEvent event) {
-        synchronized (mutex) {
-            //System.out.println("Process: " + event.getType());
-            mutex.notify();
-        }
-    }
+	/**
+	 * Producer-Consumer queue class this class create a queue in the ZooKeeper
+	 * as a Znode such that the object, in this specific case Strings, can be
+	 * put in a queue and consumed one by one with out Overlapping one another
+	 * 
+	 * Yosef
+	 */
+	static public class Queue extends SyncPrimitive {
 
-    /**
-     * Barrier
-     */
-    static public class Barrier extends SyncPrimitive {
-        int size;
-        String name;
+		/**
+		 * Constructor of producer-consumer queue
+		 *
+		 * Create a queue if it does not exist already
+		 * 
+		 * @param address
+		 * @param name
+		 */
+		Queue(String address, String name) {
+			super(address);
+			this.root = name;
+			// Create ZK node name
+			if (zk != null) {
+				try {
+					Stat s = zk.exists(root, false);
+					if (s == null) {
+						zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
+								CreateMode.PERSISTENT);
+					}
+				} catch (KeeperException e) {
+					System.out
+							.println("Keeper exception when instantiating queue: "
+									+ e.toString());
+				} catch (InterruptedException e) {
+					System.out.println("Interrupted exception");
+				}
+			}
+		}
 
-        /**
-         * Barrier constructor
-         *
-         * @param address
-         * @param root
-         * @param size
-         */
-        Barrier(String address, String root, int size) {
-            super(address);
-            this.root = root;
-            this.size = size;
+		/**
+		 * Add element to the queue. get a String and encode it into a Byte
+		 * buffer then put it as a node son of the queue node or in short a
+		 * "element"
+		 *
+		 * @param string
+		 * @return
+		 */
 
-            // Create barrier node
-            if (zk != null) {
-                try {
-                    Stat s = zk.exists(root, false);
-                    if (s == null) {
-                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                                CreateMode.PERSISTENT);
-                    }
-                } catch (KeeperException e) {
-                    System.out
-                            .println("Keeper exception when instantiating queue: "
-                                    + e.toString());
-                } catch (InterruptedException e) {
-                    System.out.println("Interrupted exception");
-                }
-            }
+		boolean produce(String string) throws KeeperException,
+				InterruptedException {
+			ByteBuffer b = null;
+			byte[] value = null;
+			// Encode String
+			try {
+				Charset charset = Charset.forName("UTF-8");
+				CharsetEncoder encoder = charset.newEncoder();
+				b = encoder.encode(CharBuffer.wrap(string));
+				value = new byte[b.limit()];
+				b.get(value);
 
-            // My node name
-            try {
-                name = new String(InetAddress.getLocalHost().getCanonicalHostName().toString());
-            } catch (UnknownHostException e) {
-                System.out.println(e.toString());
-            }
+			} catch (CharacterCodingException ex) {
 
-        }
+				System.out
+						.println("produce exception when codeing string to bytes: "
+								+ ex.toString());
+				return false;
+			}
 
-        /**
-         * Join barrier
-         *
-         * @return
-         * @throws KeeperException
-         * @throws InterruptedException
-         */
+			String result = zk.create(root + "/element", value,
+					Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+			System.out.println("Znode created: " + result);
 
-        boolean enter() throws KeeperException, InterruptedException{
-            zk.create(root + "/" + name, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                    CreateMode.EPHEMERAL_SEQUENTIAL);
-            while (true) {
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
+			return true;
+		}
 
-                    if (list.size() < size) {
-                        mutex.wait();
-                    } else {
-                        return true;
-                    }
-                }
-            }
-        }
+		/**
+		 * Remove first element from the queue.
+		 *
+		 * get the byte array from the queue and decode it into a String then
+		 * return it to the consumer
+		 * 
+		 * @param string
+		 * @return
+		 *
+		 * @return
+		 * @throws KeeperException
+		 * @throws InterruptedException
+		 */
+		String consume() throws KeeperException, InterruptedException {
+			String retvalue = "";
+			Stat stat = null;
 
-        /**
-         * Wait until all reach barrier
-         *
-         * @return
-         * @throws KeeperException
-         * @throws InterruptedException
-         */
+			// Get the first element available
+			while (true) {
+				synchronized (mutex) {
+					List<String> list = zk.getChildren(root, true);
+					if (list.size() == 0) {
+						System.out.println("Going to wait");
+						mutex.wait();
+					} else {
+						Integer min = new Integer(list.get(0).substring(7));
+						String nodeName = list.get(0);
+						for (String s : list) {
+							Integer tempValue = new Integer(s.substring(7));
+							if (tempValue < min) {
+								min = tempValue;
+								nodeName = s;
 
-        boolean leave() throws KeeperException, InterruptedException{
-            zk.delete(root + "/" + name, 0);
-            while (true) {
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
-                        if (list.size() > 0) {
-                            mutex.wait();
-                        } else {
-                            return true;
-                        }
-                    }
-                }
-        }
-    }
+							}
 
-    /**
-     * Producer-Consumer queue
-     */
-    static public class Queue extends SyncPrimitive {
+						}
+						System.out.println("Node picked: " + root + "/"
+								+ nodeName);
+						byte[] b = zk.getData(root + "/" + nodeName, false,
+								stat);
+						zk.delete(root + "/" + nodeName, 0);
 
-        /**
-         * Constructor of producer-consumer queue
-         *
-         * @param address
-         * @param name
-         */
-        Queue(String address, String name) {
-            super(address);
-            this.root = name;
-            // Create ZK node name
-            if (zk != null) {
-                try {
-                    Stat s = zk.exists(root, false);
-                    if (s == null) {
-                        zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE,
-                                CreateMode.PERSISTENT);
-                    }
-                } catch (KeeperException e) {
-                    System.out
-                            .println("Keeper exception when instantiating queue: "
-                                    + e.toString());
-                } catch (InterruptedException e) {
-                    System.out.println("Interrupted exception");
-                }
-            }
-        }
-
-        /**
-         * Add element to the queue.
-         *
-         * @param string
-         * @return
-         */
-
-        boolean produce(String string) throws KeeperException, InterruptedException{
-            ByteBuffer b = null;
-            byte[] value = null;
-            // Encode String
-            	  try {
-            	    Charset charset=Charset.forName("UTF-8");
-            	    CharsetEncoder encoder=charset.newEncoder();
-            	    b=encoder.encode(CharBuffer.wrap(string));
-            	    value=new byte[b.limit()];
-            	    b.get(value);
-            	   
-            	  }
-            	 catch (  CharacterCodingException ex) {
-            	   
-            		 System.out
-                     .println("produce exception when codeing string to bytes: "
-                             + ex.toString());
-            		 return false;
-            	  }
-                    
-            zk.create(root + "/element", value, Ids.OPEN_ACL_UNSAFE,
-                        CreateMode.PERSISTENT_SEQUENTIAL);
-
-            return true;
-        }
-
-
-        /**
-         * Remove first element from the queue.
-         *
-         * @return
-         * @throws KeeperException
-         * @throws InterruptedException
-         */
-        String consume() throws KeeperException, InterruptedException{
-            String retvalue = "";
-            Stat stat = null;
-
-            // Get the first element available
-            while (true) {
-                synchronized (mutex) {
-                    List<String> list = zk.getChildren(root, true);
-                    if (list.size() == 0) {
-                        System.out.println("Going to wait");
-                        mutex.wait();
-                    } else {
-                        Integer min = new Integer(list.get(0).substring(7));
-                        for(String s : list){
-                            Integer tempValue = new Integer(s.substring(7));
-                            //System.out.println("Temporary value: " + tempValue);
-                            if(tempValue < min) min = tempValue;
-                        }
-                        System.out.println("Temporary value: " + root + "/element" + min);
-                        byte[] b = zk.getData(root + "/element" + min,
-                                    false, stat);
-                        zk.delete(root + "/element" + min, 0);
-                        
-                        //decode the string
-                        ByteBuffer buffer = ByteBuffer.wrap(b);
-                        Charset charset=Charset.forName("UTF-8");
-                	    CharsetDecoder decoder=charset.newDecoder();	
-                        try {
+						// decode the string
+						ByteBuffer buffer = ByteBuffer.wrap(b);
+						Charset charset = Charset.forName("UTF-8");
+						CharsetDecoder decoder = charset.newDecoder();
+						try {
 							retvalue = decoder.decode(buffer).toString();
 						} catch (CharacterCodingException e) {
 							System.out
-		                     .println("consume exception when decodeing bytes to string: "
-		                             + e.toString());
+									.println("Consume exception when decodeing bytes to string: "
+											+ e.toString());
 							return null;
-							
-						}
-                        
 
-                        return retvalue;
-                    }
-                }
-            }
-        }
-    }
+						}
+
+						return retvalue;
+					}
+				}
+			}
+		}
+	}
 }
--- src/main.java
+++ src/main.java
@@ -2,48 +2,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.proto.WatcherEvent;
-
-
-
-
-
 
 public class main {
 
 	public static void main(String[] args) {
 		try {
-			
+
+			// Get the file with the text to process and put the lines into a
+			// list
 			System.out.println("Please enter the text file path: ");
-			
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(
+					System.in));
 			String filePath = new String(buffer.readLine());
 			buffer.close();
 			buffer = new BufferedReader(new FileReader(filePath));
-			List<String> lines = new ArrayList<String>(); 
-			String line = buffer.readLine(); 
-			while(line != null){ 
-			    lines.add(line);
-			    line = buffer.readLine(); 
-			    
+			List<String> lines = new ArrayList<String>();
+			String line = buffer.readLine();
+			while (line != null) {
+				lines.add(line);
+				line = buffer.readLine();
+
 			}
-			
-			String address= "127.0.0.1:2181";
-			Watcher watcher = null;
-			SyncPrimitive.Queue queue = new SyncPrimitive.Queue(address, "/app1");
-			
-			
-			
-			for (Iterator<String> iterator = lines.iterator(); iterator.hasNext();) {
+
+			// create or get the TEXTS queue in ZooKeeper
+			// the address is for running locally 
+			String address = "127.0.0.1:2181";
+			SyncPrimitive.Queue queue = new SyncPrimitive.Queue(address,
+					"/TEXTS");
+
+			// For each line create an element with the line-string
+			for (Iterator<String> iterator = lines.iterator(); iterator
+					.hasNext();) {
 				String string = (String) iterator.next();
 				try {
 					queue.produce(string);
@@ -54,10 +48,9 @@ public class main {
 				}
 				System.out.println(string);
 				System.out.println();
-				
+
 			}
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
